26fd629bdd313bb11f0320950bf2668c5c2daacc
AdamWarfa <Adamwarfadk@gmail.com>
2023-02-21 21:18:51 +0100

    Respawns

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ function fewPoints() {
   document.querySelector("#enemy25Container").removeEventListener("mousedown", fewPoints);
   document.querySelector("#enemy25Container").classList.add("paused");
   document.querySelector("#enemy25").classList.add("dead");
+  document.querySelector("#enemy25Container").addEventListener("animationend", enemyReset);
+
   incrementPointsFew();
 }
 
@@ -23,6 +25,8 @@ function morePoints() {
   document.querySelector("#enemy100Container").removeEventListener("mousedown", morePoints);
   document.querySelector("#enemy100Container").classList.add("paused");
   document.querySelector("#enemy100").classList.add("dead");
+  document.querySelector("#enemy100Container").addEventListener("animationend", enemyReset);
+
   incrementPointsMore();
 }
 
@@ -30,6 +34,8 @@ function manyPoints() {
   document.querySelector("#enemy50Container").removeEventListener("mousedown", manyPoints);
   document.querySelector("#enemy50Container").classList.add("paused");
   document.querySelector("#enemy50").classList.add("dead");
+  document.querySelector("#enemy50Container").addEventListener("animationend", enemyReset);
+
   incrementPointsMany();
 }
 
@@ -56,6 +62,7 @@ function planetBig() {
   document.querySelector("#planet1Container").removeEventListener("mousedown", planetBig);
   document.querySelector("#planet1Container").classList.add("paused");
   document.querySelector("#planet1").classList.add("zoom_in");
+  document.querySelector("#planet1Container").addEventListener("animationend", planetReset);
   decrementLives();
 }
 
@@ -63,6 +70,7 @@ function planetSmall() {
   document.querySelector("#planet2Container").removeEventListener("mousedown", planetSmall);
   document.querySelector("#planet2Container").classList.add("paused");
   document.querySelector("#planet2").classList.add("zoom_in");
+  document.querySelector("#planet2Container").addEventListener("animationend", planetReset);
   decrementLives();
 }
 
@@ -76,4 +84,68 @@ function displayDecrementLives() {
   document.querySelector(`#battery${lives + 1}`).classList.remove("active-battery");
   document.querySelector(`#battery${lives + 1}`).classList.add("broken-battery");
   document.querySelector(`#point-counter`).textContent = points;
+}
+
+function enemyReset() {
+  //fjern event listener for alle 3 enemies
+  document.querySelector("#enemy25Container").removeEventListener("animationend", enemyReset);
+  document.querySelector("#enemy50Container").removeEventListener("animationend", enemyReset);
+  document.querySelector("#enemy100Container").removeEventListener("animationend", enemyReset);
+
+  // fjern dødsanimationerne
+  document.querySelector("#enemy25Container").classList.remove("paused");
+  document.querySelector("#enemy25").classList.remove("dead");
+
+  document.querySelector("#enemy50Container").classList.remove("paused");
+  document.querySelector("#enemy50").classList.remove("dead");
+
+  document.querySelector("#enemy100Container").classList.remove("paused");
+  document.querySelector("#enemy100").classList.remove("dead");
+
+  //reset initial animations
+  document.querySelector("#enemy25Container").classList.remove("flyleft");
+  document.querySelector("#enemy25Container").classList.offsetWidth;
+  document.querySelector("#enemy25Container").classList.add("flyleft");
+
+  document.querySelector("#enemy50Container").classList.remove("falling");
+  document.querySelector("#enemy50Container").classList.offsetWidth;
+  document.querySelector("#enemy50Container").classList.add("falling");
+  document.querySelector("#enemy50").classList.remove("close");
+  document.querySelector("#enemy50").classList.offsetWidth;
+  document.querySelector("#enemy50").classList.add("close");
+
+  document.querySelector("#enemy100Container").classList.remove("flyright");
+  document.querySelector("#enemy100Container").classList.offsetWidth;
+  document.querySelector("#enemy100Container").classList.add("flyright");
+
+  // Gør enemies clickable igen, som vi gjorde i startfunktionen
+  document.querySelector("#enemy25Container").addEventListener("mousedown", fewPoints);
+  document.querySelector("#enemy100Container").addEventListener("mousedown", morePoints);
+  document.querySelector("#enemy50Container").addEventListener("mousedown", manyPoints);
+}
+
+function planetReset() {
+  //fjern event listener for begge planeter
+  document.querySelector("#planet1Container").removeEventListener("animationend", enemyReset);
+  document.querySelector("#planet2Container").removeEventListener("animationend", enemyReset);
+
+  // fjern dødsanimationerne
+  document.querySelector("#planet1Container").classList.remove("paused");
+  document.querySelector("#planet1").classList.remove("zoom_in");
+
+  document.querySelector("#planet2Container").classList.remove("paused");
+  document.querySelector("#planet2").classList.remove("zoom_in");
+
+  //reset initial animations
+  document.querySelector("#planet1Container").classList.remove("driftright");
+  document.querySelector("#planet1Container").classList.offsetWidth;
+  document.querySelector("#planet1Container").classList.add("driftright");
+
+  document.querySelector("#planet2Container").classList.remove("driftleft");
+  document.querySelector("#planet2Container").classList.offsetWidth;
+  document.querySelector("#planet2Container").classList.add("driftleft");
+
+  // Gør planets clickable igen, som vi gjorde i startfunktionen
+  document.querySelector("#planet1Container").addEventListener("mousedown", planetBig);
+  document.querySelector("#planet2Container").addEventListener("mousedown", planetSmall);
 }
